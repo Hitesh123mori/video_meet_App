2ae0a2c177903ce087b1ff88433e3fdf11df9f58
harsh <harshmori0521@gmail.com>
2024-11-05 11:20:04 +0530

    resolved

--- lib/screens/signin/sign_in.dart
+++ lib/screens/signin/sign_in.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:zoom_clone/custom_widgets/button.dart';
 import 'package:zoom_clone/screens/home_screen.dart';
 import '../../Pallate.dart';
@@ -13,7 +13,6 @@ import '../greet_screen.dart';
 import '../login_screen.dart';
 import '../splash_screen.dart';
 
-
 class SignInOption extends StatefulWidget {
   const SignInOption({super.key});
 
@@ -24,58 +23,58 @@ class SignInOption extends StatefulWidget {
 class _SignInOptionState extends State<SignInOption> {
   TextEditingController eController = TextEditingController();
   TextEditingController pController = TextEditingController();
-
   final GlobalKey<FormState> _formKey2 = GlobalKey<FormState>();
 
   bool isButtonEnabled2 = false;
+  bool isLoading = false; // Loading state variable
 
-  // sign in with google
-  void  handlegooglebutton() {
-      // Dialogs.showProgressBar(context);
-      Api.signInWithGoogle(context).then((user) async {
-        // Navigator.pop(context);
-        if (user != null) {
-          bool userExists = await Api.userExistsGoogle();
-          if (userExists) {
-            Navigator.pushReplacement(context, SizeTransition4(HomeScreen()));
-          } else {
-            await Api.createUserGoogle().then((value) => Navigator.pushReplacement(context, SizeTransition4(HomeScreen()))) ;
-
-          }
-        }
+  // Sign in with Google
+  void handlegooglebutton() {
+    setState(() {
+      isLoading = true;
+    });
+    Api.signInWithGoogle(context).then((user) async {
+      setState(() {
+        isLoading = false;
       });
-
-  }
-
-  // signin with email and password
-
-  Future<void> login() async {
-
-      try {
-        // Attempt to sign in
-        await Api.signIn(
-          context,
-          eController.text,
-          pController.text,
-        );
-        User? user = Api.auth.currentUser;
-        if (user != null) {
-          bool userExists = await Api.userExistsEmail(user.uid);
-          if (userExists) {
-            Navigator.pushReplacement(context, SizeTransition4(HomeScreen()));
-          }
+      if (user != null) {
+        bool userExists = await Api.userExistsGoogle();
+        if (userExists) {
+          Navigator.pushReplacement(context, SizeTransition4(HomeScreen()));
+        } else {
+          await Api.createUserGoogle().then(
+                  (value) => Navigator.pushReplacement(context, SizeTransition4(HomeScreen())));
         }
-      } catch (error) {
-
       }
+    });
   }
 
+  // Sign in with email and password
+  Future<void> login() async {
+    setState(() {
+      isLoading = true; // Start loading
+    });
+    try {
+      await Api.signIn(context, eController.text, pController.text);
+      User? user = Api.auth.currentUser;
+      if (user != null) {
+        bool userExists = await Api.userExistsEmail(user.uid);
+        if (userExists) {
+          Navigator.pushReplacement(context, SizeTransition4(HomeScreen()));
+        }
+      }
+    } catch (error) {
+      // Handle error
+    } finally {
+      setState(() {
+        isLoading = false; // Stop loading
+      });
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-
-    // Add listeners to text controllers
     eController.addListener(updateButtonState);
     pController.addListener(updateButtonState);
   }
@@ -88,154 +87,152 @@ class _SignInOptionState extends State<SignInOption> {
 
   @override
   void dispose() {
-    // Remove listeners when the widget is disposed
-    //
     eController.removeListener(updateButtonState);
     pController.removeListener(updateButtonState);
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    mq  = MediaQuery.of(context).size ;
+    mq = MediaQuery.of(context).size;
 
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Scaffold(
-            backgroundColor: Colors.grey.shade100,
-            appBar: AppBar(
-              elevation: 0.2,
-              backgroundColor: AppColors.theme['secondaryColor'],
-              centerTitle: true,
-              leading: IconButton(
-                icon: Icon(
-                  Icons.keyboard_arrow_left,
-                  size: 40,
-                  color: AppColors.theme['secondaryTextColor'],
-                ),
-                onPressed: () {
-                  Navigator.push(context, SizeTransition5(LoginScreen()));
-                },
+      debugShowCheckedModeBanner: false,
+      home: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          backgroundColor: Colors.grey.shade100,
+          appBar: AppBar(
+            elevation: 0.2,
+            backgroundColor: AppColors.theme['secondaryColor'],
+            centerTitle: true,
+            leading: IconButton(
+              icon: Icon(
+                Icons.keyboard_arrow_left,
+                size: 40,
+                color: AppColors.theme['secondaryTextColor'],
               ),
-              title: Text(
-                "Sign In",
-                style: TextStyle(color: AppColors.theme['secondaryTextColor'],fontWeight: FontWeight.bold),
-              ),
+              onPressed: () {
+                Navigator.push(context, SizeTransition5(LoginScreen()));
+              },
             ),
-            body: Form(
-              key: _formKey2,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Container(
-                      color: Colors.grey.shade100,
-                      height: 60,
-                      width: mq.width * 1,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 22,
-                          horizontal: 20,
-                        ),
-                        child: Text(
-                          "ENTER YOUR EMAIL ADDRESS",
-                          style: TextStyle(color: Colors.grey.shade600),
-                        ),
-                      ),
-                    ),
-                    Divider(height: 0.2,),
-                    customField(
-                      hintText: 'Email',
-                      controller: eController,
-                      isNumber: false,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter your email';
-                        }
-                        return null;
-                      },
-                    ),
-                    Divider(height: 0.2,),
-                    customField(
-                      hintText: 'Password',
-                      controller: pController,
-                      isNumber: false,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter your password';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 20,),
-                    customButton(
-                      onPressed: isButtonEnabled2
-                          ? () {
-                        if (_formKey2.currentState!.validate()) {
-
-                          login();
-
-                        }
-                      }
-                          : () {}, // An empty callback if the button is disabled
-                      text: 'Sign In',
-                      textColor: isButtonEnabled2
-                          ? AppColors.theme['primaryTextColor']
-                          : Colors.grey.shade600,
-                      buttonColor: isButtonEnabled2
-                          ? AppColors.theme['primaryColor']
-                          : AppColors.theme['buttonColor2'],
-                    ),
-                    SizedBox(height: 10,),
-                    Padding(
-                      padding:  EdgeInsets.only(right :mq.width*0.56),
-                      child: TextButton(
-                          onPressed: (){},
-                          child: Text("Forget Password?")
-                      ),
-                    ),
-                    SizedBox(height: 10,),
-                    Container(
-                      color: Colors.grey.shade100,
-                      height: 60,
-                      width: mq.width * 1,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 22,
-                          horizontal: 20,
-                        ),
-                        child: Text(
-                          "OTHER SIGN IN METHODS",
-                          style: TextStyle(color: Colors.grey.shade600),
-                        ),
-                      ),
-                    ),
-                    authContainer(
-                      onPressed: ()  {
-                        handlegooglebutton();
-                      },
-                      text: 'Continue with Google',
-                      path: 'assets/images/google.png',
-
-                    ),
-                    SizedBox(height: 10,),
-                    authContainer(
-                      onPressed: () {  },
-                      text: 'Continue with Apple',
-                      path: 'assets/images/apple-logo.png',
-
-                    ),
-                    SizedBox(height: 10,),
-                    authContainer(
-                      onPressed: () {  },
-                      text: 'Continue with facebook',
-                      path: 'assets/images/facebook.png',
-                    ),
-                  ],
-                ),
-              ),
+            title: Text(
+              "Sign In",
+              style: TextStyle(color: AppColors.theme['secondaryTextColor'], fontWeight: FontWeight.bold),
             ),
           ),
-        ));
+          body: Stack(
+            children: [
+              Form(
+                key: _formKey2,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        color: Colors.grey.shade100,
+                        height: 60,
+                        width: mq.width,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 20),
+                          child: Text(
+                            "ENTER YOUR EMAIL ADDRESS",
+                            style: TextStyle(color: Colors.grey.shade600),
+                          ),
+                        ),
+                      ),
+                      Divider(height: 0.2),
+                      customField(
+                        hintText: 'Email',
+                        controller: eController,
+                        isNumber: false,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          return null;
+                        },
+                      ),
+                      Divider(height: 0.2),
+                      customField(
+                        hintText: 'Password',
+                        controller: pController,
+                        isNumber: false,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      customButton(
+                        onPressed: isButtonEnabled2
+                            ? () {
+                          if (_formKey2.currentState!.validate()) {
+                            login();
+                          }
+                        }
+                            : () {},
+                        text: 'Sign In',
+                        textColor: isButtonEnabled2
+                            ? AppColors.theme['primaryTextColor']
+                            : Colors.grey.shade600,
+                        buttonColor: isButtonEnabled2
+                            ? AppColors.theme['primaryColor']
+                            : AppColors.theme['buttonColor2'],
+                      ),
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: EdgeInsets.only(right: mq.width * 0.56),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text("Forget Password?"),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        color: Colors.grey.shade100,
+                        height: 60,
+                        width: mq.width,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 20),
+                          child: Text(
+                            "OTHER SIGN IN METHODS",
+                            style: TextStyle(color: Colors.grey.shade600),
+                          ),
+                        ),
+                      ),
+                      authContainer(
+                        onPressed: handlegooglebutton,
+                        text: 'Continue with Google',
+                        path: 'assets/images/google.png',
+                      ),
+                      SizedBox(height: 10),
+                      authContainer(
+                        onPressed: () {},
+                        text: 'Continue with Apple',
+                        path: 'assets/images/apple-logo.png',
+                      ),
+                      SizedBox(height: 10),
+                      authContainer(
+                        onPressed: () {},
+                        text: 'Continue with Facebook',
+                        path: 'assets/images/facebook.png',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              if (isLoading)
+                Center(
+                  child: CircularProgressIndicator(
+                    color:AppColors.theme['primaryColor'],
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/signup/sign_up_options.dart
+++ lib/screens/signup/sign_up_options.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:zoom_clone/custom_widgets/button.dart';
-import 'package:zoom_clone/screens/greet_screen.dart';
 import 'package:zoom_clone/screens/home_screen.dart';
 import 'package:zoom_clone/screens/login_screen.dart';
 import '../../Pallate.dart';
@@ -11,8 +10,6 @@ import '../../effects/Transition2.dart';
 import '../../effects/transition4.dart';
 import '../../effects/transition5.dart';
 import '../../resources/Api.dart';
-import '../detail_screen.dart';
-import '../splash_screen.dart';
 
 class SignUpOption extends StatefulWidget {
   const SignUpOption({super.key});
@@ -22,47 +19,56 @@ class SignUpOption extends StatefulWidget {
 }
 
 class _SignUpOptionState extends State<SignUpOption> {
-
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
 
   final GlobalKey<FormState> _formKey2 = GlobalKey<FormState>();
 
   bool isButtonEnabled2 = false;
-
   bool isRegistering = false;
 
-// sign up with google
-  void handlegooglebutton(){
-    Dialogs.showProgressBar(context);
+  // Sign up with Google
+  void handleGoogleButton() {
+    setState(() {
+      isRegistering = true;
+    });
     Api.signInWithGoogle(context).then((user) async {
-      Navigator.pop(context);
+      setState(() {
+        isRegistering = false;
+      });
       if (user != null) {
         bool userExists = await Api.userExistsGoogle();
         if (userExists) {
           Navigator.pushReplacement(context, SizeTransition4(HomeScreen()));
         } else {
-          await Api.createUserGoogle().then((value) => Navigator.pushReplacement(context, SizeTransition4(HomeScreen()))) ;
+          await Api.createUserGoogle().then((value) {
+            Navigator.pushReplacement(context, SizeTransition4(HomeScreen()));
+          });
         }
       }
     });
   }
 
-
-  //sign up with email-password :
-
+  // Sign up with email-password
   Future<void> signUp() async {
-      try {
-        await Api.signUp(
-          context,
-          emailController.text,
-          passController.text,
-        ) ;
-        print("#hello done register") ;
-        await Navigator.pushReplacement(context,SizeTransition2(HomeScreen()));
-
-      } catch (error) {
-      }
+    setState(() {
+      isRegistering = true;
+    });
+    try {
+      await Api.signUp(
+        context,
+        emailController.text,
+        passController.text,
+      );
+      await Navigator.pushReplacement(context, SizeTransition2(HomeScreen()));
+    } catch (error) {
+      // Handle error
+      print(error);
+    } finally {
+      setState(() {
+        isRegistering = false;
+      });
+    }
   }
 
   @override
@@ -80,15 +86,16 @@ class _SignUpOptionState extends State<SignUpOption> {
 
   @override
   void dispose() {
-    // Remove listeners when the widget is disposed
-    //
     emailController.removeListener(updateButtonState);
     passController.removeListener(updateButtonState);
+    emailController.dispose();
+    passController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    mq  = MediaQuery.of(context).size ;
+    Size mq = MediaQuery.of(context).size;
 
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
@@ -108,129 +115,125 @@ class _SignUpOptionState extends State<SignUpOption> {
         ),
         title: Text(
           "Sign Up",
-          style: TextStyle(color: AppColors.theme['secondaryTextColor'],fontWeight: FontWeight.bold),
+          style: TextStyle(color: AppColors.theme['secondaryTextColor'], fontWeight: FontWeight.bold),
         ),
       ),
-      body: Form(
-        key: _formKey2,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                color: Colors.grey.shade100,
-                height: 60,
-                width: mq.width * 1,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 22,
-                    horizontal: 20,
+      body: Stack(
+        children: [
+          Form(
+            key: _formKey2,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.grey.shade100,
+                    height: 60,
+                    width: mq.width,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 20),
+                      child: Text(
+                        "ENTER YOUR EMAIL ADDRESS",
+                        style: TextStyle(color: Colors.grey.shade600),
+                      ),
+                    ),
                   ),
-                  child: Text(
-                    "ENTER YOUR EMAIL ADDRESS",
-                    style: TextStyle(color: Colors.grey.shade600),
+                  Divider(height: 0.2),
+                  customField(
+                    hintText: 'Email',
+                    controller: emailController,
+                    isNumber: false,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
                   ),
-                ),
-              ),
-              Divider(height: 0.2,),
-              customField(
-                hintText: 'Email',
-                controller: emailController,
-                isNumber: false,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-              ),
-              Divider(height: 0.2,),
-              customField(
-                hintText: 'Password',
-                controller: passController,
-                isNumber: false,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  if (value.length < 6) {
-                    return 'Password must be at least 6 characters';
-                  }
-                  final passwordRegex = RegExp(r'^(?=.*[!@#\$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,}$');
-                  if (!passwordRegex.hasMatch(value)) {
-                    return 'Password must contain at least one special character, one lowercase letter, and one uppercase letter';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20,),
-              customButton(
-                onPressed: isButtonEnabled2
-                    ? () {
-                  if (_formKey2.currentState!.validate()) {
-                      signUp().then((value) {
-                        print("user created succesufully");
-                        Navigator.pushReplacement(context,SizeTransition4(GreetScreen()));
-                      });
-                  }
-                }
-                    : () {},
-                text: 'Sign Up',
-                textColor: isButtonEnabled2
-                    ? AppColors.theme['primaryTextColor']
-                    : Colors.grey.shade600,
-                buttonColor: isButtonEnabled2
-                    ? AppColors.theme['primaryColor']
-                    : AppColors.theme['buttonColor2'],
-              ),
-              SizedBox(height: 10,),
-              Padding(
-                padding:  EdgeInsets.only(right :mq.width*0.56),
-                child: TextButton(
-                    onPressed: (){},
-                    child: Text("Forget Password?")
-                ),
-              ),
-              SizedBox(height: 10,),
-              Container(
-                color: Colors.grey.shade100,
-                height: 60,
-                width: mq.width * 1,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 22,
-                    horizontal: 20,
+                  Divider(height: 0.2),
+                  customField(
+                    hintText: 'Password',
+                    controller: passController,
+                    isNumber: false,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      if (value.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      final passwordRegex = RegExp(r'^(?=.*[!@#\$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,}$');
+                      if (!passwordRegex.hasMatch(value)) {
+                        return 'Password must contain at least one special character, one lowercase letter, and one uppercase letter';
+                      }
+                      return null;
+                    },
                   ),
-                  child: Text(
-                    "OTHER SIGN UP METHODS",
-                    style: TextStyle(color: Colors.grey.shade600),
+                  SizedBox(height: 20),
+                  customButton(
+                    onPressed: isButtonEnabled2
+                        ? () {
+                      if (_formKey2.currentState!.validate()) {
+                        signUp();
+                      }
+                    }
+                        : () {},
+                    text: 'Sign Up',
+                    textColor: isButtonEnabled2
+                        ? AppColors.theme['primaryTextColor']
+                        : Colors.grey.shade600,
+                    buttonColor: isButtonEnabled2
+                        ? AppColors.theme['primaryColor']
+                        : AppColors.theme['buttonColor2'],
                   ),
-                ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: EdgeInsets.only(right: mq.width * 0.56),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text("Forget Password?"),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    color: Colors.grey.shade100,
+                    height: 60,
+                    width: mq.width,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 20),
+                      child: Text(
+                        "OTHER SIGN UP METHODS",
+                        style: TextStyle(color: Colors.grey.shade600),
+                      ),
+                    ),
+                  ),
+                  authContainer(
+                    onPressed: handleGoogleButton,
+                    text: 'Continue with Google',
+                    path: 'assets/images/google.png',
+                  ),
+                  SizedBox(height: 10),
+                  authContainer(
+                    onPressed: () {},
+                    text: 'Continue with Apple',
+                    path: 'assets/images/apple-logo.png',
+                  ),
+                  SizedBox(height: 10),
+                  authContainer(
+                    onPressed: () {},
+                    text: 'Continue with Facebook',
+                    path: 'assets/images/facebook.png',
+                  ),
+                ],
               ),
-              authContainer(
-                onPressed: ()  {
-                  handlegooglebutton() ;
-                },
-                text: 'Continue with Google',
-                path: 'assets/images/google.png',
-
-              ),
-              SizedBox(height: 10,),
-              authContainer(
-                onPressed: () {  },
-                text: 'Continue with Apple',
-                path: 'assets/images/apple-logo.png',
-
-              ),
-              SizedBox(height: 10,),
-              authContainer(
-                onPressed: () {  },
-                text: 'Continue with facebook',
-                path: 'assets/images/facebook.png',
-
-              ),
-            ],
+            ),
           ),
-        ),
+          if (isRegistering)
+            Center(
+              child: CircularProgressIndicator(
+                color: AppColors.theme['primaryColor'],
+              ),
+            ),
+        ],
       ),
     );
   }
